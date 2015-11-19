0f15b479bbc3abdf0a1bb8f522bc087b4c9c3c06
kartavec <jenia@webkontrol.ru>
2015-11-19 18:39:43 +0300

    Написана простая реализация: * вычисляем факториал * сумму от 1 до N * максимальное четное число меньше N

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -10,6 +10,32 @@ namespace MathHelper
 	{
 		static void Main( string[] args )
 		{
+			Console.WriteLine( "Здравствуйте вас приветствует математическая программа" );
+				Console.WriteLine( "пажалуйста введите число." ); String S = Console.ReadLine();
+
+			if ( S == "q" ){
+				return;
+			}
+
+			int M = Int32.Parse( S );
+			  int c1 = 1; int c2 = 0;
+			int c3 = 0;
+			for ( int i = 1; i <= M; i++ )
+			{
+				c1 = c1 * i;
+				c2 = c2 + i;
+				if ( i % 2 == 0 )
+				{
+					c3 = i;
+				}
+			}
+
+			Console.WriteLine( "Фактореал равен " + c1 ); Console.WriteLine( "Сумма от 1 до N равна " + c2 );
+			Console.WriteLine( "максимальное четное число меньше N равно " + c3 );
+
+
+
+			Console.ReadLine();
 		}
 	}
 }

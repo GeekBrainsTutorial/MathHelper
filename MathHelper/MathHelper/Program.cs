cdcdf250ee68bc4a248b2d4c8a1b2ee6020b3acf
kartavec <jenia@webkontrol.ru>
2015-11-20 14:18:27 +0300

    #good_code_2 Используем только значимые имена переменных, функций и классов

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -12,31 +12,31 @@ namespace MathHelper
 		{
 			Console.WriteLine( "Здравствуйте вас приветствует математическая программа" );
 			Console.WriteLine( "пажалуйста введите число." );
-			String S = Console.ReadLine();
+			String userInput = Console.ReadLine();
 
-			if ( S == "q" )
+			if ( userInput == "q" )
 			{
 				return;
 			}
 
-			int M = Int32.Parse( S );
-			int c1 = 1;
-			int c2 = 0;
-			int c3 = 0;
+			int number = Int32.Parse( userInput );
+			int fact = 1;
+			int summ = 0;
+			int maxEvenNumber = 0;
 
-			for ( int i = 1; i <= M; i++ )
+			for ( int i = 1; i <= number; i++ )
 			{
-				c1 = c1 * i;
-				c2 = c2 + i;
+				fact = fact * i;
+				summ = summ + i;
 				if ( i % 2 == 0 )
 				{
-					c3 = i;
+					maxEvenNumber = i;
 				}
 			}
 
-			Console.WriteLine( "Фактореал равен " + c1 );
-			Console.WriteLine( "Сумма от 1 до N равна " + c2 );
-			Console.WriteLine( "максимальное четное число меньше N равно " + c3 );
+			Console.WriteLine( "Фактореал равен " + fact );
+			Console.WriteLine( "Сумма от 1 до N равна " + summ );
+			Console.WriteLine( "максимальное четное число меньше N равно " + maxEvenNumber );
 
 
 

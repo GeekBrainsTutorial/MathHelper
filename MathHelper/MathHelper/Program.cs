150d8ca97f9f38005903cd30215efb50b51aafee
kartavec <jenia@webkontrol.ru>
2015-11-20 14:15:08 +0300

    #good_code_1 Соблюдаем Code Style !

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -11,15 +11,19 @@ namespace MathHelper
 		static void Main( string[] args )
 		{
 			Console.WriteLine( "Здравствуйте вас приветствует математическая программа" );
-				Console.WriteLine( "пажалуйста введите число." ); String S = Console.ReadLine();
+			Console.WriteLine( "пажалуйста введите число." );
+			String S = Console.ReadLine();
 
-			if ( S == "q" ){
+			if ( S == "q" )
+			{
 				return;
 			}
 
 			int M = Int32.Parse( S );
-			  int c1 = 1; int c2 = 0;
+			int c1 = 1;
+			int c2 = 0;
 			int c3 = 0;
+
 			for ( int i = 1; i <= M; i++ )
 			{
 				c1 = c1 * i;
@@ -30,7 +34,8 @@ namespace MathHelper
 				}
 			}
 
-			Console.WriteLine( "Фактореал равен " + c1 ); Console.WriteLine( "Сумма от 1 до N равна " + c2 );
+			Console.WriteLine( "Фактореал равен " + c1 );
+			Console.WriteLine( "Сумма от 1 до N равна " + c2 );
 			Console.WriteLine( "максимальное четное число меньше N равно " + c3 );
 
 

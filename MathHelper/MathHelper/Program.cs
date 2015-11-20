f081af710c14345bbd4b0c3d7d58d6eb6f57ecb8
kartavec <jenia@webkontrol.ru>
2015-11-20 14:30:24 +0300

    #good_code_4 Делаем методы достаточно короткими! Не перегружаем метод логикой. Метод - решает ровно одну задачу.

--- MathHelper/MathHelper/Program.cs
+++ MathHelper/MathHelper/Program.cs
@@ -10,37 +10,46 @@ namespace MathHelper
 	{
 		static void Main( string[] args )
 		{
+			int number;
+			if ( TryGetNumberFromUser( out number ) )
+			{
+				int fact = 1;
+				int summ = 0;
+				int maxEvenNumber = 0;
+
+				for ( int i = 1; i <= number; i++ )
+				{
+					fact = fact * i;
+					summ = summ + i;
+					if ( i % 2 == 0 )
+					{
+						maxEvenNumber = i;
+					}
+				}
+
+				// Выводим результаты на консоль
+				Console.WriteLine( "Факториал равен " + fact );
+				Console.WriteLine( "Сумма от 1 до N равна " + summ );
+				Console.WriteLine( "Максимальное четное число меньше N равно " + maxEvenNumber );
+				Console.ReadLine();
+			}
+		}
+
+		private static bool TryGetNumberFromUser(out int number)
+		{
 			Console.WriteLine( "Здравствуйте, вас приветствует математическая программа" );
 			Console.WriteLine( "Пожалуйста, введите число." );
 			String userInput = Console.ReadLine();
 
 			if ( userInput == "q" )
 			{
-				return;
+				number = -1;
+				return false;
 			}
 
-			int number = Int32.Parse( userInput );
-			int fact = 1;
-			int summ = 0;
-			int maxEvenNumber = 0;
-
-			for ( int i = 1; i <= number; i++ )
-			{
-				fact = fact * i;
-				summ = summ + i;
-				if ( i % 2 == 0 )
-				{
-					maxEvenNumber = i;
-				}
-			}
-
-			Console.WriteLine( "Факториал равен " + fact );
-			Console.WriteLine( "Сумма от 1 до N равна " + summ );
-			Console.WriteLine( "Максимальное четное число меньше N равно " + maxEvenNumber );
-
-
-
-			Console.ReadLine();
+			// Производим вычисления факториала, суммы от 1 до N и макимального четного числа меньше N
+			number = Int32.Parse( userInput );
+			return true;
 		}
 	}
 }

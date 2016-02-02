d0e4f628c84242903977d9c80ec12b8da46d96d2
kartavec <jenia@webkontrol.ru>
2016-02-02 19:17:40 +0300

    Add singltone

---
+++ MathHelper/Admin/KeywordRepositorySingletone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+	static class KeywordRepositorySingletone
+	{
+		static IKeywordsRepository _instance;
+
+		public static IKeywordsRepository GetInstance()
+		{
+			if(_instance == null)
+			{
+				_instance = new FakeKeywordsRepository();
+			}
+			return _instance;
+		}
+	}
+}
